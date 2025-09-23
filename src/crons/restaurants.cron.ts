34710bda54182d607e260a9473f5219c75d54eb0
Anthony LALBA <a.lalba@agoravita.com>
2025-09-23 17:40:22 +0200

    Rollback on crons

--- src/crons/restaurants.cron.ts
+++ src/crons/restaurants.cron.ts
@@ -1,7 +1,7 @@
 import { restaurantService } from '../services/restaurant.service';
 import cron from 'node-cron';
 
-const restaurantCron = cron.schedule('* * * * *', () => { // every day at 2:00 AM
+const restaurantCron = cron.schedule('0 2 * * *', () => { // every day at 2:00 AM
     console.log('Cron job executed: Truncating restaurants data');
     restaurantService.truncate();
 });
