5d7bcc3e5fdffa8d8d16d95323b634428cc92905
Anthony LALBA <a.lalba@agoravita.com>
2025-09-26 12:03:26 +0200

    Log on crons

--- src/crons/restaurants.cron.ts
+++ src/crons/restaurants.cron.ts
@@ -3,8 +3,10 @@ import { fingerprint } from '../utils/fingerprint.utils';
 import cron from 'node-cron';
 
 const restaurantCron = cron.schedule('0 6 * * *', () => { // every day at 6:00 AM
-    console.log('Cron job executed: Truncating restaurants data');
+    console.log('Cron job executed: truncating restaurants data');
     restaurantService.truncate();
+
+    console.log('Cron job executed: clearing fingerprints');
     fingerprint.clear();
 });
 
